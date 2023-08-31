76b0695c7d1bf64cd5fe827d3dbcc5416f4a9372
Sravya <saisravyayakkati99@gmail.com>
2023-08-31 09:56:23 +0000

    updated scripts folder and appspec

---
+++ scripts/start_container.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+# Pull the Docker image from Docker Hub
+docker pull sravya037/simple-python-flask-app:latest
+
+# Run the Docker image as a container
+docker run -d -p 4000:4000 sravya037/simple-python-flask-app
+
